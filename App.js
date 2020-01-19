33f17ba4f001c4dc3db2544e85f8bb0884b943dd
swOOsh <ayushk2020@gmail.com>
2020-01-18 22:58:51 -0500

    Fixed React-Navigation 3 issues

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 // Import the screens
 import Main from './components/Main';
-import Chat from './components/Chat';
+//import Chat from './components/Chat';
 // Import React Navigation
 import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation';
 // Create the navigator
 const navigator = createStackNavigator({
   Main: { screen: Main },
-  Chat: { screen: Chat },
+  //Chat: { screen: Chat },
 });
 // Export it as the root component
-export default navigator
+const NavContainer = createAppContainer(navigator);
+export default NavContainer
